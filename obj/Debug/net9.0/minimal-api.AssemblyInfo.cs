a4fd2750fbfec400c8058d156a619f2a783d2523
DevBruno <brunoantonio.dev0016@gmail.com>
2025-09-25 20:58:15 -0300

    Definindo a classe de admistradores

--- obj/Debug/net9.0/minimal-api.AssemblyInfo.cs
+++ obj/Debug/net9.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b382e1e24a2f7f74e22c3955a1d2bd49c44ce8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbde8b85564858165f4a3150e294a3d718541026")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
