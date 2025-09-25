0f6b2f5a6dda279d7514993dee53054786cdad6c
DevBruno <brunoantonio.dev0016@gmail.com>
2025-09-25 20:57:37 -0300

    Configurando o DbContext

--- Infrastructure/DB/DbContext.cs
+++ Infrastructure/DB/DbContext.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using MinimalApi.Domain.Entities;
+
 namespace MinimalApi.Infrastructure.Db;
 
-public class DbContext
+public class DbContext : DbContext
 {
-    
+    public DbSet<Administrator> Administrators { get; set; } = default!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("string de conexão");
+    }
 }
