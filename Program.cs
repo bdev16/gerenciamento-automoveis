a7cca0063005b76434d4025d89520222fcc0d516
DevBruno <brunoantonio.dev0016@gmail.com>
2025-09-25 21:29:14 -0300

    Indica para o Dotnet qual o Contexto que vai ser utilizado e liga a conexão com o banco de dados

--- Program.cs
+++ Program.cs
@@ -1,5 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.DTOs;
+
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
+
+builder.Services.AddDbContext<DbContext>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("sqlserver")
+    );
+});
 
 app.MapGet("/", () => "Hello World!");
 
