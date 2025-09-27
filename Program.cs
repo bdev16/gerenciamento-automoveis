867fdd5e2c9bcace4b9d6f4936998c8d4d8ee3cc
DevBruno <brunoantonio.dev0016@gmail.com>
2025-09-27 20:27:42 -0300

    Muda a instanciação do builder de lugar

--- Program.cs
+++ Program.cs
@@ -4,14 +4,14 @@ using MinimalApi.Infrastructure.Db;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
-
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("sqlserver")
     );
 });
+
+var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
