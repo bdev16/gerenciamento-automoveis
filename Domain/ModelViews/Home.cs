5c815ce1896de98c63fa3ced4274762d6c3ae267
DevBruno <brunoantonio.dev0016@gmail.com>
2025-09-27 22:51:06 -0300

    Define uma classe de view

---
+++ Domain/ModelViews/Home.cs
@@ -0,0 +1,7 @@
+namespace MinimalApi.Domain.ModelViews;
+
+public struct Home
+{
+    public string Mensage { get => "Bem vindo a API de gerencimaneto de automoveis!"; }
+    public string Document { get => "/swagger"; }
+}
