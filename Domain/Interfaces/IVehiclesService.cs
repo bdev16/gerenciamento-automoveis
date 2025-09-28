27e074c5d806dd99447ac77adce26b1b8cfe4539
DevBruno <brunoantonio.dev0016@gmail.com>
2025-09-27 23:56:43 -0300

    Modifica o parametro page para poder receber valores nulos

--- Domain/Interfaces/IVehiclesService.cs
+++ Domain/Interfaces/IVehiclesService.cs
@@ -2,7 +2,7 @@ using MinimalApi.Domain.Entities;
 
 public interface IVehicleService
 {
-    List<Vehicle> All(int page, string? nome = null, string? marca = null);
+    List<Vehicle> All(int? page, string? nome = null, string? marca = null);
     Vehicle? SearchForId(int id);
     void Include(Vehicle vehicle);
     void Update(Vehicle vehicle);
