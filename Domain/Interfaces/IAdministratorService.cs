5aa0af62b04b3d20aded551c6c258813b0991770
DevBruno <brunoantonio.dev0016@gmail.com>
2025-09-30 21:51:52 -0300

    Define os métodos Include, SearchForId e All na IAdministratorService

--- Domain/Interfaces/IAdministratorService.cs
+++ Domain/Interfaces/IAdministratorService.cs
@@ -6,4 +6,7 @@ namespace MinimalApi.Dominio.Interfaces;
 public interface IAdministratorService
 {
     Administrator? Login(LoginDTO loginDTO);
+    Administrator Include(Administrator administrator);
+    Administrator? SearchForId(int id);
+    List<Administrator> All(int? page);
 }
