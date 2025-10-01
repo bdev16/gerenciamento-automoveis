270645fa16b706f015e5549aeac3902b9e2837da
DevBruno <brunoantonio.dev0016@gmail.com>
2025-09-30 23:54:20 -0300

    Define um model view para Admnistrator após Login

---
+++ Domain/ModelViews/AdministratorLogged.cs
@@ -0,0 +1,8 @@
+namespace MinimalApi.Domain.ModelViews;
+
+public record AdministratorLoggad
+{
+    public string Email { get; set; } = default!;
+    public string Perfil { get; set; } = default!;
+    public string Token { get; set; } = default!;
+}
