2400c0c0eb5535dd6f2209a00890a3b2d16e97ee
DevBruno <brunoantonio.dev0016@gmail.com>
2025-09-30 21:54:53 -0300

    Implementa os metodos All, Include e SearchForId em AdministratorService:

--- Domain/Services/AdministratorService.cs
+++ Domain/Services/AdministratorService.cs
@@ -14,6 +14,31 @@ public class AdministratorService : IAdministratorService
         _context = context;
     }
 
+    public Administrator? SearchForId(int id)
+    {
+        return _context.Administrators.Where(administrator => administrator.Id == id).FirstOrDefault();
+    }
+
+    public List<Administrator> All(int? page)
+    {
+        var query = _context.Administrators.AsQueryable();
+
+        int itensForPage = 10;
+
+        if (page != null)
+            query = query.Skip(((int)page - 1) * itensForPage).Take(itensForPage);
+
+        return query.ToList();
+    }
+
+    public Administrator Include(Administrator administrator)
+    {
+        _context.Administrators.Add(administrator);
+        _context.SaveChanges();
+
+        return administrator;
+    }
+
     Administrator? IAdministratorService.Login(LoginDTO loginDTO)
     {
         var adm = _context.Administrators.Where(a => a.Email == loginDTO.Email && a.Senha == loginDTO.Senha).FirstOrDefault();
