0ee975b836aa9d66b4272b0347ccd44102eb73c0
DevBruno <brunoantonio.dev0016@gmail.com>
2025-10-13 21:19:10 -0300

    Testa o servico de Administrator
    Verifica se o servico de administrators está funcionando corretamente.

---
+++ Test/Domain/Services/AdministratorServiceTest.cs
@@ -0,0 +1,51 @@
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using MinimalApi.Domain.Entities;
+using MinimalApi.Dominio.Services;
+using MinimalApi.Infrastructure.Db;
+
+namespace Test.Domain.Entities;
+
+[TestClass]
+public class AdministratorServiceTest
+{
+    private AppDbContext CreateContextTest()
+    {
+        var assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        var path = Path.GetFullPath(Path.Combine(assemblyPath ?? "", "..", "..", ".."));
+
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(path ?? Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+        var configuration = builder.Build();
+
+        return new AppDbContext(configuration);
+    }  
+
+    [TestMethod]
+    public void TestSaveAdministratorToBd()
+    {
+
+        // Arrange
+        var context = CreateContextTest();
+        context.Database.ExecuteSqlRaw("TRUNCATE TABLE Administrators");
+
+        var administrator = new Administrator();
+        administrator.Email = "administrador@teste.com";
+        administrator.Senha = "123456";
+        administrator.Perfil = "Adm";
+
+        var administratorService = new AdministratorService(context);
+
+        // Act
+        administratorService.Include(administrator);
+
+        // Assert
+        Assert.AreEqual(1, administratorService.All(1).Count());
+
+    }
+}
