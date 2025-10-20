6bec5b356f434811a04e83279e299bcdb34bbf63
DevBruno <brunoantonio.dev0016@gmail.com>
2025-10-20 20:03:35 -0300

    Testa o servico de Vehicles
    Verifica se o metodo Update do servico de Vehicles.

--- Test/Domain/Services/VehicleServiceTest.cs
+++ Test/Domain/Services/VehicleServiceTest.cs
@@ -2,6 +2,7 @@ using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using MinimalApi.Domain.Entities;
+using MinimalApi.DTOs;
 using MinimalApi.Infrastructure.Db;
 
 namespace Test.Domain.Services;
@@ -86,6 +87,45 @@ public class VehicleServiceTest
 
         // Assert
         Assert.AreEqual(1, vehicleService.All(1).Count());
+    }
+
+    [TestMethod]
+    public void UpdateVehicleToDB()
+    {
+        // Arrange
+        var context = CreateContextTest();
+        context.Database.ExecuteSqlRaw("TRUNCATE TABLE Vehicles;");
+
+        var vehicle = new Vehicle();
+        vehicle.Nome = "teste";
+        vehicle.Marca = "testeteste";
+        vehicle.Ano = 2000;
+
+        var vehicleNome = vehicle.Nome;
+        var vehicleMarca = vehicle.Marca;
+        var vehicleAno = vehicle.Ano;
+
+        var vehicleDTO = new VehicleDTO();
+        vehicleDTO.Nome = "teste modificado";
+        vehicleDTO.Marca = "testeteste modificado";
+        vehicleDTO.Ano = 2010;
+
+        var vehicleService = new VehicleService(context);
+
+        // Act
+        vehicleService.Include(vehicle);
+
+        vehicle.Nome = vehicleDTO.Nome;
+        vehicle.Marca = vehicleDTO.Marca;
+        vehicle.Ano = vehicleDTO.Ano;
+
+        vehicleService.Update(vehicle);
+
+        // Assert
+        Assert.IsFalse(vehicle.Nome == vehicleNome);
+        Assert.IsFalse(vehicle.Marca == vehicleMarca);
+        Assert.IsFalse(vehicle.Ano == vehicleAno);
+    
     }
 
     [TestMethod]
