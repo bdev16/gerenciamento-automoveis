8c8c304bdbbc3db5896af43169f2fdedaf2804e9
DevBruno <brunoantonio.dev0016@gmail.com>
2025-10-20 19:43:51 -0300

    Testa o servico de Vehicles
    Verifica se o metodo Delete do servico de Vehicles está funcionando.

--- Test/Domain/Services/VehicleServiceTest.cs
+++ Test/Domain/Services/VehicleServiceTest.cs
@@ -87,4 +87,36 @@ public class VehicleServiceTest
         // Assert
         Assert.AreEqual(1, vehicleService.All(1).Count());
     }
+
+    [TestMethod]
+    public void UpdateVehicleToBD()
+    {
+        // Arrange
+        var context = CreateContextTest();
+        context.Database.ExecuteSqlRaw("TRUNCATE TABLE Vehicles;");
+
+        var vehicle = new Vehicle();
+        vehicle.Nome = "teste";
+        vehicle.Marca = "testeteste";
+        vehicle.Ano = 2000;
+
+        var vehicleTwo = new Vehicle();
+        vehicleTwo.Nome = "teste02";
+        vehicleTwo.Marca = "testeteste02";
+        vehicleTwo.Ano = 2002;
+
+        var vehicleService = new VehicleService(context);
+
+        // Act
+        vehicleService.Include(vehicle);
+        vehicleService.Include(vehicleTwo);
+
+        vehicleService.Delete(vehicle);
+
+        var vehicleResult = vehicleService.SearchForId(1);
+
+        // Assert
+        Assert.IsNull(vehicleResult);
+
+    }
 }
