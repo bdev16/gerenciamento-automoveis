15ecf0be6d00bad915ec56c92ac97f4807cef331
aluno <aluno@embarquedigital.portodigital.local>
2025-10-22 20:06:02 -0300

    Cria a classe Mock de AdministratorService
    Essa classe vai simular o funcionamento da classe Administrator
    Service, porém sem qualquer tipo de dependencias existentes. É uma copia
    da classe real só para ser utilizada no ambiente de testes.

---
+++ Test/Mocks/AdministratorServiceMock.cs
@@ -0,0 +1,49 @@
+using MinimalApi.Domain.Entities;
+using MinimalApi.Dominio.Interfaces;
+using MinimalApi.DTOs;
+
+namespace Test.Helpers;
+
+public class AdministratorServiceMock : IAdministratorService
+{
+    private static List<Administrator> administrators = new List<Administrator>()
+    {
+        new Administrator
+        {
+            Id = 1,
+            Email = "administrator@teste.com",
+            Senha = "123456",
+            Perfil = "Adm"
+        },
+
+        new Administrator
+        {
+            Id = 2,
+            Email = "editor@teste.com",
+            Senha = "123456",
+            Perfil = "Editor"
+        }
+    };
+
+    public List<Administrator> All(int? page)
+    {
+        return administrators;
+    }
+
+    public Administrator Include(Administrator administrator)
+    {
+        administrator.Id = administrators.Count + 1;
+        administrators.Add(administrator);
+        return administrator;
+    }
+
+    public Administrator? Login(LoginDTO loginDTO)
+    {
+        return administrators.Find(administrator => administrator.Email == loginDTO.Email && administrator.Senha == loginDTO.Senha);
+    }
+
+    public Administrator? SearchForId(int id)
+    {
+        return administrators.Find(administrator => administrator.Id == id);
+    }
+}
