f7cd2bf53ccfe35c667dbb0145d7aa7c8a6984a7
aluno <aluno@embarquedigital.portodigital.local>
2025-10-22 20:08:22 -0300

    Implementa a classe AdministratorRequestTest
    Define os metodos ClassInitialize, ClassCleanup e o metodo de testes que
    vai testar o request de login, que vai verificar se o request de login
    está funcionando da maneira correta.

--- Test/Requests/AdministratorRequestTest.cs
+++ Test/Requests/AdministratorRequestTest.cs
@@ -1,7 +1,56 @@
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Azure.Core;
+using MinimalApi.Domain.ModelViews;
+using MinimalApi.DTOs;
+using Test.Helper;
+
 namespace Test.Request;
 
 [TestClass]
 public class AdministratorRequestTest
 {
-    
+    [ClassInitialize]
+    public static void ClassInit(TestContext testContext)
+    {
+        Setup.ClassInit(testContext);
+    }
+
+    [ClassCleanup]
+    public static void ClassCleanup()
+    {
+        Setup.ClassCleanup();
+    }
+
+    [TestMethod]
+    public async Task TestLoginRequest()
+    {
+        // Arrange
+        var loginDTO = new LoginDTO
+        {
+            Email = "administrator@teste.com",
+            Senha = "123456"
+        };
+
+        var content = new StringContent(JsonSerializer.Serialize(loginDTO), Encoding.UTF8, "Application/json");
+
+        // Act
+        var response = await Setup.client.PostAsync("/Administrators/login", content);
+
+        // Assert
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+        var result = await response.Content.ReadAsStringAsync();
+        var administratorLogged = JsonSerializer.Deserialize<AdministratorLoggad>(result, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        });
+
+
+        Assert.IsNotNull(administratorLogged?.Email ?? "");
+        Assert.IsNotNull(administratorLogged?.Perfil ?? "");
+        Assert.IsNotNull(administratorLogged?.Token ?? "");
+    }
 }
