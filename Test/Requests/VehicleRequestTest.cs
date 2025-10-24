01d5c3cbfa16c799d65202169940e5955adcef25
aluno <aluno@embarquedigital.portodigital.local>
2025-10-24 20:54:20 -0300

    Define o metodo de testes TestSaveVehicleRequest
    Esse método testa o request que recebe novos registros de vehicles.

--- Test/Requests/VehicleRequestTest.cs
+++ Test/Requests/VehicleRequestTest.cs
@@ -1,9 +1,18 @@
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using Azure.Core;
+using Microsoft.AspNetCore.Http.Features;
+using MinimalApi.Domain.Entities;
+using MinimalApi.Domain.ModelViews;
+using MinimalApi.DTOs;
 using Test.Helper;
 
 namespace Test.Request;
 
 [TestClass]
-public class VehicleRequest
+public class VehicleRequestTest
 {
     [ClassInitialize]
     public static void ClassInit(TestContext testContext)
@@ -16,4 +25,59 @@ public class VehicleRequest
     {
         Setup.ClassCleanup();
     }
+
+    [TestMethod]
+    public async Task TestSaveVehicleRequest()
+    {
+        // Arrange
+        var loginDTO = new LoginDTO
+        {
+            Email = "administrator@teste.com",
+            Senha = "123456"
+        };
+
+        var contentLogin = new StringContent(JsonSerializer.Serialize(loginDTO), Encoding.UTF8, "Application/json");
+
+        var vehicleDTO = new VehicleDTO
+        {
+            Nome = "teste02",
+            Marca = "testeMarca02",
+            Ano = 2002
+        };
+
+        var contentVehicle = new StringContent(JsonSerializer.Serialize(vehicleDTO), Encoding.UTF8, "Application/json");
+
+        // Act
+        var responseLoginRequest = await Setup.client.PostAsync("/Administrators/login", contentLogin);
+
+        // Assert
+        Assert.AreEqual(HttpStatusCode.OK, responseLoginRequest.StatusCode);
+
+        var resultResponseLoginRequest = await responseLoginRequest.Content.ReadAsStringAsync();
+        var administratorLogged = JsonSerializer.Deserialize<AdministratorLoggad>(resultResponseLoginRequest, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        });
+
+        Assert.IsNotNull(administratorLogged?.Email ?? "");
+        Assert.IsNotNull(administratorLogged?.Perfil ?? "");
+        Assert.IsNotNull(administratorLogged?.Token ?? "");
+
+        Setup.client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", administratorLogged?.Token);
+
+        var responseSaveVehicleRequest = await Setup.client.PostAsync("/vehicles", contentVehicle);
+
+        Assert.AreEqual(HttpStatusCode.Created, responseSaveVehicleRequest.StatusCode);
+
+        var resultResponseSaveVehicleRequest = await responseSaveVehicleRequest.Content.ReadAsStringAsync();
+        var vehicleCreatedResult = JsonSerializer.Deserialize<Vehicle>(resultResponseSaveVehicleRequest, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        });
+
+        Assert.IsNotNull(vehicleCreatedResult?.Nome ?? "");
+        Assert.IsNotNull(vehicleCreatedResult?.Marca ?? "");
+        Assert.IsNotNull(vehicleCreatedResult?.Ano ?? 0);
+    }
+
 }
