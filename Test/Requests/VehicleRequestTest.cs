e45ef033c016e23d1fbfe1158650dfad49aa7bae
aluno <aluno@embarquedigital.portodigital.local>
2025-10-24 20:01:28 -0300

    Define a class VehicleRequestTest
    Essa classe vai testar todos os requests vehicles.

---
+++ Test/Requests/VehicleRequestTest.cs
@@ -0,0 +1,19 @@
+using Test.Helper;
+
+namespace Test.Request;
+
+[TestClass]
+public class VehicleRequest
+{
+    [ClassInitialize]
+    public static void ClassInit(TestContext testContext)
+    {
+        Setup.ClassInit(testContext);
+    }
+
+    [ClassCleanup]
+    public static void ClassCleanup()
+    {
+        Setup.ClassCleanup();
+    }
+}
