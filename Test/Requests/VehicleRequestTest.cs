1b3e8a357c2a721dfdcddfaf23cb5b47dbaf3850
DevBruno <brunoantonio.dev0016@gmail.com>
2025-10-26 16:41:57 -0300

    Define o método de Teste TaskGetEspecificVehicleRequest
    Esse método tem o intuito de testar o endpoint de vehicles que retorna
    um vehicle especifico por meio do seu ID.

--- Test/Requests/VehicleRequestTest.cs
+++ Test/Requests/VehicleRequestTest.cs
@@ -80,4 +80,48 @@ public class VehicleRequestTest
         Assert.IsNotNull(vehicleCreatedResult?.Ano ?? 0);
     }
 
+    [TestMethod]
+    public async Task TestGetEspecificVehicleRequest()
+    {
+        // Arrage
+        var loginDTO = new LoginDTO
+        {
+            Email = "administrator@teste.com",
+            Senha = "123456"
+        };
+
+        var contentLogin = new StringContent(JsonSerializer.Serialize(loginDTO), Encoding.UTF8, "Application/json");
+
+        // Act
+        var responseLoginRequest = await Setup.client.PostAsync("/Administrators/login", contentLogin);
+
+        // Assert
+        Assert.AreEqual(HttpStatusCode.OK, responseLoginRequest.StatusCode);
+
+        var resultResponseLoginRequest = await responseLoginRequest.Content.ReadAsStringAsync();
+        var administratorLogged = JsonSerializer.Deserialize<AdministratorLoggad>(resultResponseLoginRequest, new JsonSerializerOptions {
+            PropertyNameCaseInsensitive = true
+        });
+
+        Assert.IsNotNull(administratorLogged?.Email ?? "");
+        Assert.IsNotNull(administratorLogged?.Perfil ?? "");
+        Assert.IsNotNull(administratorLogged?.Token ?? "");
+
+        Setup.client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", administratorLogged?.Token);
+
+        var responseGetEspecificVehicleRequest = await Setup.client.GetAsync("/vehicles/1");
+
+        Assert.AreEqual(HttpStatusCode.OK, responseGetEspecificVehicleRequest.StatusCode);
+
+        var resultResponseGetEspecificVehicleRequest = await responseGetEspecificVehicleRequest.Content.ReadAsStringAsync();
+        var vehicle = JsonSerializer.Deserialize<Vehicle>(resultResponseGetEspecificVehicleRequest, new JsonSerializerOptions{
+            PropertyNameCaseInsensitive = true
+        });
+
+        Assert.IsNotNull(vehicle?.Nome ?? "");
+        Assert.IsNotNull(vehicle?.Marca ?? "");
+        Assert.IsNotNull(vehicle?.Ano ?? 0);
+
+    }
+
 }
